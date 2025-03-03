0ff2c93403f2ab8bd66b7b8d74ad954b6252753d
USERLI <hliek@connect.ust.hk>
2025-03-03 16:31:58 +0800

    Lab1

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -15,22 +15,32 @@
  */
 package com.example.tiptime
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -38,6 +48,17 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tiptime.ui.theme.TipTimeTheme
 import java.text.NumberFormat
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Switch
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,39 +75,190 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
+fun getBmiCategory(bmi: Double): String {
+    return when {
+        bmi < 18.5 -> "Underweight"
+        bmi in 18.5..24.9 -> "Normal weight"
+        bmi in 25.0..29.9 -> "Overweight"
+        bmi in 30.0..34.9 -> "Moderate obesity"
+        bmi in 35.0..39.9 -> "Severe obesity"
+        else -> "Very severe or morbid obesity"
+    }
+}
+@Composable
+fun EditNumberField(
+    @StringRes label: Int,
+    value: String,
+    keyboardOptions: KeyboardOptions,
+    onValueChanged: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = value,
+        singleLine = true,
+        modifier = modifier,
+        onValueChange = onValueChanged,
+        label = { Text(stringResource(label)) },
+        keyboardOptions = keyboardOptions
+    )
+}
+@Composable
+fun RoundTheTipRow(
+    type:Int,
+    roundUp: Boolean,
+    onRoundUpChanged: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        if(type == 0){
+            Text(text = stringResource(R.string.ft))
+        }
+        else{
+            Text(text = stringResource(R.string.lb))
+        }
+        Switch(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.End),
+            checked = roundUp,
+            onCheckedChange = onRoundUpChanged
+        )
+    }
+}
 @Composable
 fun TipTimeLayout() {
+    var heightInput by remember { mutableStateOf("") }
+    var weightInput by remember { mutableStateOf("") }
+    var weight = weightInput.toDoubleOrNull() ?: 0.0
+    var height = heightInput.toDoubleOrNull() ?: 0.0
+    //var roundUp by remember { mutableStateOf(false) }
+    var bmi by remember { mutableStateOf(0.0) }
+    var isCalculated by remember { mutableStateOf(false) }
+    var isError by remember { mutableStateOf(true) }
+    var useft by remember { mutableStateOf(false) }
+    var uselb by remember { mutableStateOf(false) }
+    if(useft){
+        height *= 30.48
+    }
+    if(uselb){
+        weight *=0.453592
+    }
+
     Column(
         modifier = Modifier
             .statusBarsPadding()
             .padding(horizontal = 40.dp)
-            .safeDrawingPadding(),
+            .safeDrawingPadding()
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Text(
-            text = stringResource(R.string.calculate_tip),
+            text = stringResource(R.string.calculate_bmi),
             modifier = Modifier
                 .padding(bottom = 16.dp, top = 40.dp)
                 .align(alignment = Alignment.Start)
         )
-        Text(
-            text = stringResource(R.string.tip_amount, "$0.00"),
-            style = MaterialTheme.typography.displaySmall
+        EditNumberField(
+            label = R.string.height,
+            value = heightInput,
+            onValueChanged = { heightInput = it },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth()
         )
+        EditNumberField(
+            label = R.string.weight,
+            value = weightInput,
+            onValueChanged = { weightInput = it },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth()
+        )
+        RoundTheTipRow(
+            type = 0,
+            roundUp = useft,
+            onRoundUpChanged = { useft = it },
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        RoundTheTipRow(
+            type = 1,
+            roundUp = uselb,
+            onRoundUpChanged = { uselb = it },
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Button(
+            onClick = {
+                bmi = calculateBmi(height, weight) // 点击按钮时计算 BMI
+                isCalculated = true // 标记已计算
+                Log.d("BMI_CALCULATION", "BMI: $bmi")
+                if(bmi == 0.0){
+                    isError = true
+                }
+                else{
+                    isError = false
+                }
+            },
+            modifier = Modifier
+                .padding(bottom = 32.dp)
+                .fillMaxWidth()
+        ) {
+            Text(text = stringResource(R.string.calculate))
+        }
+        Button(
+            onClick = {
+                heightInput = "" // 清除身高输入
+                weightInput = "" // 清除体重输入
+                isCalculated = false // 重置计算结果
+                isError = true
+            },
+            modifier = Modifier
+                .padding(bottom = 32.dp)
+                .fillMaxWidth()
+        ) {
+            Text(text = stringResource(R.string.clear))
+        }
+        if (isCalculated) {
+            if(isError){
+                Text(
+                    text = stringResource(R.string.error_message),
+                    fontSize = 24.sp, // 设置字体大小为 24sp
+                    style = MaterialTheme.typography.displaySmall
+                )
+            }
+            else{
+                Text(
+                    text = stringResource(R.string.your_bmi_is, bmi),
+                    style = MaterialTheme.typography.displaySmall
+                )
+            }
+
+        }
+        if(!isError){
+            val answer = getBmiCategory(bmi)
+            Text(
+                text = answer,
+                style = MaterialTheme.typography.displaySmall
+            )
+        }
         Spacer(modifier = Modifier.height(150.dp))
     }
 }
+@SuppressLint("DefaultLocale")
+private fun calculateBmi(height: Double, weight: Double): Double {
 
-/**
- * Calculates the tip based on the user input and format the tip amount
- * according to the local currency.
- * Example would be "$10.00".
- */
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
-    val tip = tipPercent / 100 * amount
-    return NumberFormat.getCurrencyInstance().format(tip)
+    if(height>0.0 && weight >0.0){
+        val bmi = weight*10000 / (height * height)
+        return bmi
+    }
+    return 0.0
 }
 
 @Preview(showBackground = true)
